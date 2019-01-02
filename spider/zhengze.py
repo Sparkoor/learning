f2cbca405e4f6379ba2d2ba6b84a06b1ea39b01e
884831651@qq.com <19940528zh>
2019-01-02 20:24:55 +0800

    使用线程池了

--- spider/zhengze.py
+++ spider/zhengze.py
@@ -1,10 +1,10 @@
 import re
 
 str = '[\'\n                    合租 · 恒大名都 3室1厅 600元                  \']'
-pattern = re.compile('\[|\]')
-ss = re.match(pattern, str)
-print(ss.group(0))
+# pattern = re.compile('\[|\]')
+# ss = re.match(pattern, str)
+# print(ss.group(0))
 
-aa = str.replace('[\'', '')
-bb = aa.replace('\']', '')
-print(bb.strip())
+aa = str.replace('[\'', '')\
+    .replace('\']', '').strip()
+print(aa)
