2d0122a9e91abd826e4da910d960c957dbdaa503
zwp <884831651@qq.com>
2019-02-28 21:35:07 +0800

    adaboost元算法
    Signed-off-by: zwp <884831651@qq.com>

--- machineLearning/cha8/regression.py
+++ machineLearning/cha8/regression.py
@@ -49,3 +49,92 @@ def standRegress(dataArr, classLabel):
     # note:求逆
     ws = xTx.I * (xMat.T * yMat)
     return ws
+
+
+def lwlr(testPoint, xArr, yArr, k=1.0):
+    """
+    局部加权线性回归
+    :param testPoint: 测试的一个样本
+    :param xArr: 训练的点
+    :param yArr: 结果
+    :param k: 参数k
+    :return:
+    """
+    testMat = np.mat(testPoint)
+    xMat = np.mat(xArr)
+    yMat = np.mat(yArr)
+    m = xMat.shape[0]
+    # 生成对角阵
+    weight = np.mat(np.eye(m))
+    for i in range(m):
+        diffMat = testMat - xMat[i, :]
+        weight[i, i] = np.exp(diffMat * diffMat.T / (-2 * k ** 2))
+    xTx = xMat.T * (weight * xMat)
+    # 零矩阵不存在颗逆矩阵
+    if np.linalg.det(xTx) == 0.0:
+        logger.error("行列式为零")
+        return
+    ws = xTx.I * (xMat.T * (weight * yMat))
+    return testMat * ws
+
+
+def lwlrTest(testArr, xArr, yArr, k=1.0):
+    """
+    局部加权回归测试函数
+    :param testArr:
+    :param xArr:
+    :param yArr:
+    :param k:
+    :return:
+    """
+    m = testArr.shape[0]
+    yHat = np.zeros(m)
+    for i in range(m):
+        yHat[i] = lwlr(testArr[i], xArr, yArr, k)
+    return yHat
+
+
+def regressErrorr(yHat, yArr):
+    """
+    计算误差
+    :param yHat:
+    :param yArr:
+    :return:
+    """
+    return ((yHat - yArr) ** 2).sum()
+
+
+def ridgeRegress(xMat, yMat, lam=0.2):
+    """
+    岭回归算法
+    :param xMat:
+    :param yMat:
+    :param lam:
+    :return: X.T*X+lam*I
+    """
+    xTx = xMat.T * xMat
+    denom = xTx + lam * np.eye(xTx.shape[1])
+    if np.linalg.det(denom) == 0.0:
+        logger.error("行列式为0")
+        return
+    # todo:验证不同的相乘顺序
+    return denom.I * (xMat.T * yMat)
+
+
+def ridgeTest(xArr, yArr):
+    """
+    岭回归的测试
+    :param xArr:
+    :param yArr:
+    :return:
+    """
+    xMat = np.mat(xArr)
+    yMat = np.mat(yArr)
+    # 均值
+    yMean = np.mean(yArr, 0)
+    yMat = yMat - yMean
+    xMean = np.mean(xMat, 0)
+    # 求方差
+    xVar = np.var(xMean, 0)
+    # note:特征值的标准化：所有特征都减去各自的均值并除以方差
+    xMat = (xMat - xMean) / xVar
